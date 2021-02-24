5660814331e318f40e1d7b6e0f9ef450f635b40e
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-24 12:47:46 +0530

    [Shubham] refactor:Implemented Context with Proper formating

--- src/Components/Context/Context.jsx
+++ src/Components/Context/Context.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-
-
 const bookListContext = React.createContext();
-const cartItemLength = React.createContext();
 
-export default (bookListContext ,cartItemLength)
-
+export default bookListContext;
