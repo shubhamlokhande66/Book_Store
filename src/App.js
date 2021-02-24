fff18a1346a79e18fd8da04374ed56a206958984
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-24 12:58:30 +0530

    [Shubham] refactor: Removed unwanted Routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-
-import React from 'react';
-import './App.scss';
-import {BrowserRouter as Routing, Route, Switch} from 'react-router-dom'
-
-import Home from './Components/Header/Header';
-import Signup from './Components/pages/signup/signup'
-import Login from './Components/pages/login/login'
-import Displaybook from './Components/DisplayBooks/Displaybook'
-import Dashboard from './Components/Dashborad/Dashboard'
-import Cart from "./Components/Cart/Cart"
-import Placce from "./Components/Orderplace/OrderPlace"
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Routing, Route, Switch } from "react-router-dom";
+import Dashboard from "./Components/Dashborad/Dashboard";
 const App = () => {
   return (
     <Routing>
       <div>
         <Switch>
-        <Route path="/" exact component={Dashboard}></Route>
-        <Route path="/signup" component={Signup}></Route>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/Displaybook" component={Displaybook}></Route>
-        <Route path="/cart" component={Cart}></Route>
-        <Route path="/dashboard" component={Dashboard}></Route>
-        <Route path="/Place" component={Placce}></Route>
+          <Route path="/dashboard" component={Dashboard}></Route>
         </Switch>
       </div>
-      </Routing>
+    </Routing>
   );
 };
 
