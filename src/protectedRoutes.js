cdb0bf6c59bdbd2b195ef025822051f6ee7be086
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-24 12:49:54 +0530

    [Shubham] refactor:Implemented ProtectedRoutes with Proper formating

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import {Route, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
 let authenticated = false;
 
-const  isAuthenticated = () => {
-    if(localStorage.getItem("bookStoreToken") === null){
-      authenticated = false;
-    }
-    else{
-      authenticated = true;
-    }
+const isAuthenticated = () => {
+  if (localStorage.getItem("bookStoreToken") === null) {
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
   return authenticated;
-}
+};
 
-export default function ProtectedRoutes({component: Component, ...rest}){
-    return (
-        <Route {...rest} render = {(props) => {
-            if(isAuthenticated()){
-                return <Component {...props}/>
-            }
-            else{
-            }
-            
-        }} />
-    )
+export default function ProtectedRoutes({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (isAuthenticated()) {
+          return <Component {...props} />;
+        } else {
+        }
+      }}
+    />
+  );
 }
