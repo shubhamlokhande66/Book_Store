9680e56666ee5ea5024fddb2d3f22ab3a0f945eb
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-24 12:26:14 +0530

    [Shubham] refactor: Removed unwanted code and Space with proper Formatting

--- src/Components/pages/login/login.jsx
+++ src/Components/pages/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import userServices from "../../../Services/userServices";
-import './login.scss'
+import "./login.scss";
 const regexValidateEmail = new RegExp(
   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$$/
 );
@@ -9,150 +9,148 @@ const regexvalidatePassword = new RegExp(
 );
 
 class Login extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      showPassword: false,
+      errorValid: {
+        email: false,
+        password: false,
+      },
+      enable: true,
+      errors: {
         email: "",
         password: "",
-        showPassword: false,
-        errorValid: {
-          email: false,
-          password: false,
-        },
-        enable: true,
-       errors: {
-          email: "",
-          password: "",
-  
-        }
+      },
+    };
+  }
 
-
-      };
-      
+  onchangeEmail = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+    let errors = this.state.errors;
+    let validate = false;
+    if (!regexValidateEmail.test(this.state.email)) {
+      errors.email = "Please Enter Valid Email Eg:shubhamlokhande@gmail.com";
+      validate = true;
+    } else {
+      errors.email = "";
     }
-
-
-
-    onchangeEmail=(event)=>  {
-      this.setState({
-           email: event.target.value,
-         });
-         let errors = this.state.errors;
-         let validate = false;
-       if (!regexValidateEmail.test(this.state.email)) {
-           errors.email = "Please Enter Valid Email Eg:shubhamlokhande@gmail.com";
-           validate = true;
-       } else {
-           errors.email = "";
-       }  this.setState({
-           errorValid: { email: validate },
-           errors: { email: errors.email },
-         });
-       
-     };
-     onchangePassword =(event)=> {
-      this.setState({
-           password: event.target.value,
-         });
-         let errors = this.state.errors;
-         let validate = false;
-       if (!regexvalidatePassword.test(this.state.password)) {
-           errors.password = "Please Enter Valid Password Eg:Shubham@123";
-           validate = true;
-       } else {
-           errors.password = "";
-       }this.setState({
-           
-           errorValid: { password: validate },
-           errors: { password: errors.password },
-         });
-       
-     };
-    
-
-    
-
-    onSubmit = () => { 
-    
-      let data = {
-        email: this.state.email,
-        password: this.state.password,
-      };
-  
-      userServices
-        .userlogin(data)
-        .then((data) => {
-          console.log(
-            "Login successful" + JSON.stringify(data.data.result.accessToken)
-          );
-          localStorage.setItem("bookStoreToken", data.data.result.accessToken);
-        
-          localStorage.setItem("email", data.data.email);
-          setTimeout(() => {
-            this.props.history.push("/");
-          }, 2000);
-          
-          
-        }) 
-        .catch((err) => {
-          console.log("Registration Error" + err);
-        });
-       
-      
+    this.setState({
+      errorValid: { email: validate },
+      errors: { email: errors.email },
+    });
+  };
+  onchangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+    let errors = this.state.errors;
+    let validate = false;
+    if (!regexvalidatePassword.test(this.state.password)) {
+      errors.password = "Please Enter Valid Password Eg:Shubham@123";
+      validate = true;
+    } else {
+      errors.password = "";
     }
- 
+    this.setState({
+      errorValid: { password: validate },
+      errors: { password: errors.password },
+    });
+  };
 
-    render() {
-  return (
-    <div className="signup">
-    <form  autocomplete="off">
-     
+  onSubmit = () => {
+    let data = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    userServices
+      .userlogin(data)
+      .then((data) => {
+        console.log(
+          "Login successful" + JSON.stringify(data.data.result.accessToken)
+        );
+        localStorage.setItem("bookStoreToken", data.data.result.accessToken);
+
+        localStorage.setItem("email", data.data.email);
+        setTimeout(() => {
+          this.props.history.push("/");
+        }, 2000);
+      })
+      .catch((err) => {
+        console.log("Registration Error" + err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="signup">
+        <form autocomplete="off">
           <div class="form-group">
-      <label for="exampleInputEmail1">Email Id</label>
-      <input 
-                    value={this.state.email}
-                    onChange={this.onchangeEmail}
-
-                    variant="outlined"
-                    label="email"
-                
-                    placeholder="email"
-                   
-                    type="text"
-                    type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-             
-                    <span className='error-output'> {this.state.errors["email"]}</span>
+            <label for="exampleInputEmail1">Email Id</label>
+            <input
+              value={this.state.email}
+              onChange={this.onchangeEmail}
+              variant="outlined"
+              label="email"
+              placeholder="email"
+              type="text"
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <span className="error-output"> {this.state.errors["email"]}</span>
           </div>
           <div class="form-group">
-      <label for="exampleInputEmail1">Password</label>
-      <input 
-          id="password"
-          variant="outlined"
-          type="password"
-          label="Password"
-          fullWidth
-          value={this.state.password}
-          onChange={this.onchangePassword}
-      
-          placeholder="password"
-         
-          type="text"
-          type={this.state.showPassword ? "text" : "password"}type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Password"/>
-   
-                    <span className='error-output'> {this.state.errors["password"]}</span>
+            <label for="exampleInputEmail1">Password</label>
+            <input
+              id="password"
+              variant="outlined"
+              type="password"
+              label="Password"
+              fullWidth
+              value={this.state.password}
+              onChange={this.onchangePassword}
+              placeholder="password"
+              type="text"
+              type={this.state.showPassword ? "text" : "password"}
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter Password"
+            />
+            <span className="error-output">
+              {" "}
+              {this.state.errors["password"]}
+            </span>
           </div>
-          <button type="button" class="btn1 btn-danger" onClick={(e) => this.onSubmit(e)}>Login</button>
-<div className="or">
-  <h6>-------------OR--------------</h6>
-</div>
-
-
-<button type="button" class="Facebook btn-primary">Facebook</button>
-<button type="button" class="Google btn-outline-secondary">Google</button>
-          </form>
+          <button
+            type="button"
+            class="btn1 btn-danger"
+            onClick={(e) => this.onSubmit(e)}
+          >
+            Login
+          </button>
+          <div className="or">
+            <h6>-------------OR--------------</h6>
           </div>
-  
-  );
-};
+
+          <button type="button" class="Facebook btn-primary">
+            Facebook
+          </button>
+          <button type="button" class="Google btn-outline-secondary">
+            Google
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-export default (Login);
+export default Login;
