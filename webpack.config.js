c269674e1559bf77924448d2d08407c3687a8f9a
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-24 12:51:38 +0530

    [Shubham] refactor:Implemented Webpack.confi file with Proper formating

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,73 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.[hash].js',
-        publicPath: '/'
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-                
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localsConvention: "camelCase",
+              sourceMap: true,
             },
-
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localsConvention: 'camelCase',
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|jpg|svg|jfif)$/i,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]',
-                },
-            },
-
-
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-
-            
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'dist/index.html',
-            // favicon: '/dist/download.png',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-    ],
-    devServer: {
-        host: 'localhost',
-        port: port,
-        historyApiFallback: true,
-        open: true,
-        hot: true
-    },
-    resolve: {
-
-        extensions: ["*", ".js", ".jsx", ".css", ".scss",".png",".jpg",".jfif"],
-        alias: {
-            "react-dom": "@hot-loader/react-dom",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|jpg|svg|jfif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
         },
-    }
-    // Webpack configuration goes here
+      },
+
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "dist/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".css", ".scss", ".png", ".jpg", ".jfif"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
 };
