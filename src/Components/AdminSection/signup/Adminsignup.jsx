23ee6523c41b3685de24ffd14ca1a37f3c7f9698
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-03-03 08:15:38 +0530

    [Shubham] refactor. Admin Signup page with proper alignment and formate

--- src/Components/AdminSection/signup/Adminsignup.jsx
+++ src/Components/AdminSection/signup/Adminsignup.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import './Adminsignup.scss';
+import "./Adminsignup.scss";
 import userServices from "../AdminService/AdminUserService";
 import Adminlogo from "../../../Assets/admin.png";
 const regexValidateEmail = new RegExp(
@@ -12,10 +12,6 @@ const regexvalidatePassword = new RegExp(
 const regexvalidateName = new RegExp(/^[A-Z]{1}[a-z]{3,}$/);
 const regexvalidateMobile = new RegExp(/^.{10,}$/);
 
-
-
-
-
 class Signup extends Component {
   constructor(props) {
     super(props);
@@ -23,241 +19,256 @@ class Signup extends Component {
       fullName: "",
       email: "",
       password: "",
-      mobilenumber:"",
+      mobilenumber: "",
       showPassword: false,
       errorValid: {
         fullName: false,
         email: false,
         password: false,
-        mobilenumber:false,
+        mobilenumber: false,
       },
       enable: true,
-     
+
       errors: {
         fullName: "",
         email: "",
         password: "",
-        mobilenumber:"",
+        mobilenumber: "",
       },
     };
-    
   }
 
-  
-  validate =(data) =>{
+  validate = (data) => {
     const errors = {};
-    if (!regexValidateEmail.test(data.email))
-      errors.email = "Invalid email";
+    if (!regexValidateEmail.test(data.email)) errors.email = "Invalid email";
     return errors;
   };
-  onchangefullName=(event)=>  { 
+  onchangefullName = (event) => {
     this.setState({
       fullName: event.target.value,
-      
     });
-    console.log(event.target.value)
+    console.log(event.target.value);
     let errors = this.state.errors;
     let validate = false;
-  if (!regexvalidateName.test(this.state.fullName)) {
+    if (!regexvalidateName.test(this.state.fullName)) {
       errors.fullName = "Please Enter Valid Name Eg:Shubham";
       validate = true;
-  } else {
+    } else {
       errors.fullName = "";
-   } this.setState({
+    }
+    this.setState({
       errorValid: { fullName: validate },
-    errors: { fullName: errors.fullName },
+      errors: { fullName: errors.fullName },
     });
-  
-};
-onchangeEmail=(event)=>  {
-  this.setState({
-       email: event.target.value,
-     });
-     let errors = this.state.errors;
-     let validate = false;
-   if (!regexValidateEmail.test(this.state.email)) {
-       errors.email = " Please Enter Valid Email Eg:shubhamlokhande@gmail.com";
-       validate = true;
-   } else {
-       errors.email = "";
-   }  this.setState({
-       errorValid: { email: validate },
-       errors: { email: errors.email },
-     });
-   
- };
-
-
- onchangePassword =(event)=> {
-  this.setState({
-       password: event.target.value,
-     });
-     let errors = this.state.errors;
-     let validate = false;
-   if (!regexvalidatePassword.test(this.state.password)) {
-       errors.password = " Please Enter Valid Password Eg:Shubham@123";
-       validate = true;
-   } else {
-       errors.password = "";
-   }this.setState({
-       
-       errorValid: { password: validate },
-       errors: { password: errors.password },
-     });
-   
- };
-
- onchangeMobileNo =(event)=> {
-  this.setState({
-    mobilenumber: event.target.value,
-     });
-     let errors = this.state.errors;
-     let validate = false;
-   if (!regexvalidateMobile.test(this.state.mobilenumber)) {
-       errors.mobilenumber = "  Please Enter Valid Phone No. Enter Minimum 10 digit ";
-       validate = true;
-   } else {
-       errors.mobilenumber = "";
-   }this.setState({
-       
-       errorValid: { mobilenumber: validate },
-       errors: { mobilenumber: errors.mobilenumber },
-     });
-   
- };
- validate = (data) => {
-  const errors = {};
-  if (!regexValidateEmail.test(data.email))
-    errors.email = "Invalid email";
-  return errors;
-};
- 
- onSubmit = () => { 
-  const errors = this.validate(this.state);
-  if (errors.email || this.state.email === "") {
+  };
+  onchangeEmail = (event) => {
     this.setState({
-      errorValid: true,
-      errors: "Please Enter the Email ",
+      email: event.target.value,
     });
-  } else if (this.state.password === "") {
+    let errors = this.state.errors;
+    let validate = false;
+    if (!regexValidateEmail.test(this.state.email)) {
+      errors.email = " Please Enter Valid Email Eg:shubhamlokhande@gmail.com";
+      validate = true;
+    } else {
+      errors.email = "";
+    }
     this.setState({
-      errorValid: true,
-      errors: "Please Enter the password",
+      errorValid: { email: validate },
+      errors: { email: errors.email },
     });
-  } else {
-    let data = {
-      "fullName": this.state.fullName,
-      "email": this.state.email,
-      "password": this.state.password, 
-      "phone": this.state.mobilenumber,  
-    };
+  };
 
-    userServices
-      .AdminUserRegister(data)
-      .then((data) => {
-        console.log("registration successful" + data);
-        this.setState({
-          fullName: '',
-          email: '',
-          password: '',
-          mobilenumber: '',
+  onchangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+    let errors = this.state.errors;
+    let validate = false;
+    if (!regexvalidatePassword.test(this.state.password)) {
+      errors.password = " Please Enter Valid Password Eg:Shubham@123";
+      validate = true;
+    } else {
+      errors.password = "";
+    }
+    this.setState({
+      errorValid: { password: validate },
+      errors: { password: errors.password },
+    });
+  };
 
-        });
-        this.props.history.push("/Adminlogin");
-      }) 
-      
-      .catch((err) => {
-        console.log("Registration Error" + err);
+  onchangeMobileNo = (event) => {
+    this.setState({
+      mobilenumber: event.target.value,
+    });
+    let errors = this.state.errors;
+    let validate = false;
+    if (!regexvalidateMobile.test(this.state.mobilenumber)) {
+      errors.mobilenumber =
+        "  Please Enter Valid Phone No. Enter Minimum 10 digit ";
+      validate = true;
+    } else {
+      errors.mobilenumber = "";
+    }
+    this.setState({
+      errorValid: { mobilenumber: validate },
+      errors: { mobilenumber: errors.mobilenumber },
+    });
+  };
+  validate = (data) => {
+    const errors = {};
+    if (!regexValidateEmail.test(data.email)) errors.email = "Invalid email";
+    return errors;
+  };
+
+  onSubmit = () => {
+    const errors = this.validate(this.state);
+    if (errors.email || this.state.email === "") {
+      this.setState({
+        errorValid: true,
+        errors: "Please Enter the Email ",
       });
-     
-  
-  }
-  }
+    } else if (this.state.password === "") {
+      this.setState({
+        errorValid: true,
+        errors: "Please Enter the password",
+      });
+    } else {
+      let data = {
+        fullName: this.state.fullName,
+        email: this.state.email,
+        password: this.state.password,
+        phone: this.state.mobilenumber,
+      };
 
+      userServices
+        .AdminUserRegister(data)
+        .then((data) => {
+          console.log("registration successful" + data);
+          this.setState({
+            fullName: "",
+            email: "",
+            password: "",
+            mobilenumber: "",
+          });
+          this.props.history.push("/Adminlogin");
+        })
 
-
+        .catch((err) => {
+          console.log("Registration Error" + err);
+        });
+    }
+  };
 
   render() {
-  return (
-    <div className="signupAdmin">
-       <h3 className="AdminsignupName">Admin Registration</h3>
-    <form  autocomplete="off">
-      <div className="mainadminsignup">
-        <div class="form-group">
-      <label >Full Name</label>
-          <input 
-                  name="fullName"
-                  
-                    label="First Name"
-                    variant="outlined"
-                    value={this.state.fullName}
-                    onChange={this.onchangefullName}
-                   type="text"
-                    fullWidthtype="email" class="form-control" placeholder="Enter Full Name"/>
-                    
-                    <span className='error-output'> {this.state.errors["fullName"]}</span>
-          </div>
-          <div class="form-group">
-      <label for="exampleInputEmail1">Email Id</label>
-          <input 
-                    value={this.state.email}
-                    onChange={this.onchangeEmail}
+    return (
+      <div className="signupAdmin">
+        <h3 className="AdminsignupName">Admin Registration</h3>
+        <form autocomplete="off">
+          <div className="mainadminsignup">
+            <div class="form-group">
+              <label>Full Name</label>
+              <input
+                name="fullName"
+                label="First Name"
+                variant="outlined"
+                value={this.state.fullName}
+                onChange={this.onchangefullName}
+                type="text"
+                fullWidthtype="email"
+                class="form-control"
+                placeholder="Enter Full Name"
+              />
 
-                    variant="outlined"
-                    label="email"
-                    placeholder="email"
-                    type="text"
-                    type="email" class="form-control"  placeholder="Enter email"/>
-                  
-                    <span className='error-output'> {this.state.errors["email"]}</span>
-          </div>
-          <div class="form-group">
+              <span className="error-output">
+                {" "}
+                {this.state.errors["fullName"]}
+              </span>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email Id</label>
+              <input
+                value={this.state.email}
+                onChange={this.onchangeEmail}
+                variant="outlined"
+                label="email"
+                placeholder="email"
+                type="text"
+                type="email"
+                class="form-control"
+                placeholder="Enter email"
+              />
 
-      <label for="exampleInputPassword">Password</label>
-          <input 
-          id="password"
-          variant="outlined"
-          type="password"
-          label="Password"
-          fullWidth
-          value={this.state.password}
-          onChange={this.onchangePassword}
-          placeholder="password"
-          type="text"
-          type={this.state.showPassword ? "text" : "password"} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Password" data-toggle="password"/>
-          <span toggle="#input-pwd" class="fa fa-fw fa-eye field-icon toggle-password"></span>
- 
-                    <span className='error-output'> {this.state.errors["password"]}</span>
-          </div>
-      <div class="form-group">
-      <label for="exampleInputMobilenumber">Mobile Number</label>
-          <input 
-          
-          id="mobilenumber"
-          variant="outlined"
-          type="mobilenumber"
-          label="mobilenumber"
-          fullWidth
-          value={this.state.mobilenumber}
-          onChange={this.onchangeMobileNo}
-          placeholder="mobilenumber"
-          type="text"
-  
-          type="email" class="form-control"  placeholder="Enter Mobile Number"/>
-         
-                    <span className='error-output'> {this.state.errors["mobilenumber"]}</span>
-          </div>
-          <button type="button" class="btn1 btn-danger"  onClick={(e) => this.onSubmit(e)}>Signup</button>
+              <span className="error-output">
+                {" "}
+                {this.state.errors["email"]}
+              </span>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword">Password</label>
+              <input
+                id="password"
+                variant="outlined"
+                type="password"
+                label="Password"
+                fullWidth
+                value={this.state.password}
+                onChange={this.onchangePassword}
+                placeholder="password"
+                type="text"
+                type={this.state.showPassword ? "text" : "password"}
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter Password"
+                data-toggle="password"
+              />
+              <span
+                toggle="#input-pwd"
+                class="fa fa-fw fa-eye field-icon toggle-password"
+              ></span>
+
+              <span className="error-output">
+                {" "}
+                {this.state.errors["password"]}
+              </span>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputMobilenumber">Mobile Number</label>
+              <input
+                id="mobilenumber"
+                variant="outlined"
+                type="mobilenumber"
+                label="mobilenumber"
+                fullWidth
+                value={this.state.mobilenumber}
+                onChange={this.onchangeMobileNo}
+                placeholder="mobilenumber"
+                type="text"
+                type="email"
+                class="form-control"
+                placeholder="Enter Mobile Number"
+              />
+
+              <span className="error-output">
+                {" "}
+                {this.state.errors["mobilenumber"]}
+              </span>
+            </div>
+            <button
+              type="button"
+              class="btn1 btn-danger"
+              onClick={(e) => this.onSubmit(e)}
+            >
+              Signup
+            </button>
           </div>
 
-        
-  <img className="AdminLogo" src={Adminlogo} alt=""/>
- 
-          </form>
-          </div>
-  
-  );
-};
+          <img className="AdminLogo" src={Adminlogo} alt="" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Signup);
